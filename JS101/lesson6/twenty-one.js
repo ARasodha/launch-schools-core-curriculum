3f7ef0391bd4b053ef9f95bf167935781d7c7763
Arjun Rasodha <arjunrasodha10@gmail.com>
2021-03-10 10:59:04 -0500

    code review changes made to twenty-one, continuing through lesson 6

--- JS101/lesson6/twenty-one.js
+++ JS101/lesson6/twenty-one.js
@@ -1,4 +1,7 @@
+// My Attempt on building Twenty-One Program
 const readline = require('readline-sync');
+const MAX_TOTAL = 21;
+const DEALER_HITS_UNTIL = 17;
 
 function initializeDeck() {
   let hearts = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace'];
@@ -23,9 +26,9 @@ function evaluateCards(playerOrDealerCards) {
       sum += card;
     } else if (typeof card === 'string' && card !== 'Ace') {
       sum += 10;
-    } else if (card === 'Ace' && sum + 11 <= 21) {
+    } else if (card === 'Ace' && sum + 11 <= MAX_TOTAL) {
       sum += 11;
-    } else if (card === 'Ace' && sum + 11 > 21) {
+    } else if (card === 'Ace' && sum + 11 > MAX_TOTAL) {
       sum += 1;
     }
   });
@@ -37,7 +40,7 @@ function prompt(msg) {
 }
 
 function busted(player) {
-  return evaluateCards(player) > 21;
+  return evaluateCards(player) > MAX_TOTAL;
 }
 
 function dealCard(player, deckOfCards) {
@@ -48,7 +51,7 @@ function playAgain() {
   let answer;
   prompt('Play Again? (y/n)');
   while (true) {
-    answer = readline.question().trim();
+    answer = readline.question().trim().toLowerCase();
     if (['y', 'n'].includes(answer)) break;
     prompt('Invalid Entry. Enter either "y" or "n":');
   }
@@ -57,10 +60,36 @@ function playAgain() {
 
 function joinOr(array, delimiter = ', ', word = 'and') {
   switch (array.length) {
-    case 0: return ' ';
-    case 1: return String(array[0]);
-    case 2: return `${array[0]} ${word} ${array[1]}`;
-    default: return `${array.slice(0, array.length - 1).join(delimiter)}${delimiter}${word} ${array[array.length - 1]}`;
+    case 0:
+      return ' ';
+    case 1:
+      return String(array[0]);
+    case 2:
+      return `${array[0]} ${word} ${array[1]}`;
+    default:
+      return `${array.slice(0, array.length - 1).join(delimiter)}${delimiter}${word} ${array[array.length - 1]}`;
+  }
+}
+
+function playerHit() {
+  prompt('Hit or Stay? ("h" or "s"):');
+  let playerDecision = readline.question().toLowerCase();
+  while (!['hit', 'stay', 'h', 's'].includes(playerDecision)) {
+    prompt('Invalid entry. Enter either "(h)it" or "(s)tay":');
+    playerDecision = readline.question().toLowerCase();
+  }
+  return playerDecision;
+}
+
+function displayResult(playerCards, dealerCards) {
+  let totalPlayer = evaluateCards(playerCards);
+  let totalDealer = evaluateCards(dealerCards);
+  if (totalPlayer > totalDealer) {
+    prompt('You win!');
+  } else if (totalPlayer < totalDealer) {
+    prompt('Dealer win!');
+  } else {
+    prompt('It\'s a tie!');
   }
 }
 
@@ -68,6 +97,7 @@ function joinOr(array, delimiter = ', ', word = 'and') {
 while (true) {
   let playerCards = [];
   let dealerCards = [];
+
   console.clear();
 
   prompt('Welcome to Twenty-One!');
@@ -82,52 +112,42 @@ while (true) {
   dealCard(playerCards, deckOfCards);
   dealCard(playerCards, deckOfCards);
 
-  prompt(`You have: ${playerCards[0]} and ${playerCards[1]}`);
+  prompt(`You have: ${playerCards[0]} and ${playerCards[1]} with a total of ${evaluateCards(playerCards)}`);
 
   let playerDecision;
 
   while (true) {
-    if (busted(playerCards) || evaluateCards(playerCards) === 21) break;
+    if (busted(playerCards) || evaluateCards(playerCards) === MAX_TOTAL) break;
 
-    prompt('Hit or stay?');
-    playerDecision = readline.question().toLowerCase();
-    while (!['hit', 'stay', 'h', 's'].includes(playerDecision)) {
-      prompt('Invalid entry. Enter either "(h)it" or "(s)tay":');
-      playerDecision = readline.question().toLowerCase();
-    }
+    playerDecision = playerHit();
 
     if (['s', 'stay'].includes(playerDecision)) break;
 
     dealCard(playerCards, deckOfCards);
     evaluateCards(playerCards);
-    prompt(`You have: ${joinOr(playerCards, ', ', 'and')}`);
+    prompt(`You have: ${joinOr(playerCards, ', ', 'and')} with a total of ${evaluateCards(playerCards)}`);
   }
-
-  prompt(`The sum of the your cards is: ${evaluateCards(playerCards)}`);
 
   if (busted(playerCards)) {
     prompt('You busted! Dealer wins!'); // Add play again option
-  } else if (evaluateCards(playerCards) === 21) {
+  } else if (evaluateCards(playerCards) === MAX_TOTAL) {
     prompt('You Win!');
   } else {
     prompt('You chose to stay!');
-    while (evaluateCards(dealerCards) < 17) {
+    prompt('Dealers turn...');
+    while (evaluateCards(dealerCards) < DEALER_HITS_UNTIL) {
       if (busted(dealerCards)) break;
 
       dealCard(dealerCards, deckOfCards);
 
     }
+
+    prompt(`The dealer has ${joinOr(dealerCards, ', ', 'and')} with the total of ${evaluateCards(dealerCards)}`);
+
     if (busted(dealerCards)) {
       prompt('Dealer busted! You win!');
     } else {
-      prompt(`The sum of the dealers cards is: ${evaluateCards(dealerCards)}`);
-      if (evaluateCards(playerCards) > evaluateCards(dealerCards)) {
-        prompt('You win!');
-      } else if (evaluateCards(playerCards) < evaluateCards(dealerCards)) {
-        prompt('Dealer wins!');
-      } else {
-        prompt('It\'s a tie!');
-      }
+      displayResult(playerCards, dealerCards);
     }
   }
   if (!playAgain()) break;
